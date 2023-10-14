9345df546920088d733b71a377b1bc0194a9d4e0
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2023-10-14 19:59:05 +0200

    gitignore added. update main for optimisation.

--- main.py
+++ main.py
@@ -1,79 +1,47 @@
 import os
 import subprocess
 import logging
+import argparse
 from colorama import Fore, Style, init
 from inquirer import Checkbox, prompt
+from concurrent.futures import ThreadPoolExecutor
 
+# Constants
+SETUP_SCRIPTS = ["setup.sh", "install.sh", "configure.sh"]
+REQUIREMENT_FILES = ["requirements.txt", "deps.txt", "prerequisites.txt"]
 
 # Initialize colorama
 init()
 
-# Initialize logging
-logging.basicConfig(filename='github_tool_automation.log', level=logging.INFO)
-
-# List of GitHub repository URLs
-repositories = [
-    "https://github.com/OWASP/Amass.git",
-    "https://github.com/zaproxy/zaproxy.git",
-    "https://github.com/aboul3la/Sublist3r.git",
-    "https://github.com/FortyNorthSecurity/EyeWitness.git",
-    "https://github.com/1N3/Sn1per.git",
-    "https://github.com/projectdiscovery/subfinder.git",
-    "https://github.com/m4ll0k/infoga.git",
-    "https://github.com/michenriksen/aquatone.git",
-    "https://github.com/smicallef/spiderfoot.git",
-    "https://github.com/lanmaster53/recon-ng.git",
-]
-
-# Directory where repositories will be cloned
-base_directory = "tools"
-
-# Create the base directory if it doesn't exist
-os.makedirs(base_directory, exist_ok=True)
-
-# Color constants
-GREEN = Fore.GREEN
-RED = Fore.RED
-RESET = Style.RESET_ALL
-
 # Function to display colored messages
 def display_message(message, color):
-    print(f"{color}{message}{RESET}")
+    print(f"{color}{message}{Style.RESET_ALL}")
     logging.info(message)
 
 # Function to check for and execute setup scripts
-def run_setup_script(directory, repo_name):
-    setup_scripts = ["setup.sh", "install.sh", "configure.sh"]  # Add more if needed
-    for script in setup_scripts:
+def run_setup_scripts(directory, repo_name):
+    for script in SETUP_SCRIPTS:
         setup_script = os.path.join(directory, script)
         if os.path.exists(setup_script):
-            display_message(f"Running {script} for {repo_name}...", GREEN)
-            result = subprocess.run(["chmod", "+x", setup_script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if result.returncode == 0:
-                result = subprocess.run([setup_script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                if result.returncode == 0:
-                    display_message(f"{script} completed successfully for {repo_name}.", GREEN)
-                else:
-                    display_message(f"Error running {script} for {repo_name}.", RED)
-            else:
-                display_message(f"Error setting execute permissions for {script} in {repo_name}.", RED)
-            return True
-    return False
+            display_message(f"Running {script} for {repo_name}...", Fore.GREEN)
+            try:
+                subprocess.run(["chmod", "+x", setup_script], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                subprocess.run([setup_script], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                display_message(f"{script} completed successfully for {repo_name}.", Fore.GREEN)
+            except subprocess.CalledProcessError as e:
+                display_message(f"Error running {script} for {repo_name}. {e.stderr}", Fore.RED)
 
 # Function to check for and install requirements
 def install_requirements(directory, repo_name):
-    requirement_files = ["requirements.txt", "deps.txt", "prerequisites.txt"]  # Add more if needed
-    for req_file in requirement_files:
+    for req_file in REQUIREMENT_FILES:
         requirements_file = os.path.join(directory, req_file)
         if os.path.exists(requirements_file):
-            display_message(f"Installing requirements for {repo_name}...", GREEN)
-            result = subprocess.run(["pip", "install", "-r", requirements_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if result.returncode == 0:
-                display_message(f"Requirements installed successfully for {repo_name}.", GREEN)
-            else:
-                display_message(f"Error installing requirements for {repo_name}.", RED)
-            return True
-    return False
+            display_message(f"Installing requirements for {repo_name}...", Fore.GREEN)
+            try:
+                subprocess.run(["pip", "install", "-r", requirements_file], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                display_message(f"Requirements installed successfully for {repo_name}.", Fore.GREEN)
+            except subprocess.CalledProcessError as e:
+                display_message(f"Error installing requirements for {repo_name}. {e.stderr}", Fore.RED)
 
 # Interactive interface for selecting repositories
 def select_repositories(repos):
@@ -82,39 +50,61 @@ def select_repositories(repos):
     return answers['selected_repos']
 
 # User input validation
-def validate_input():
-    if not repositories:
-        display_message("No repositories are available for cloning.", RED)
+def validate_input(repos):
+    if not repos:
+        display_message("No repositories are available for cloning.", Fore.RED)
         exit(1)
 
-# Clone the repositories
+# Clone and set up a repository
+def clone_and_setup(repo, base_directory):
+    repo_name = repo.split("/")[-1].replace(".git", "")
+    clone_dir = os.path.join(base_directory, repo_name)
+
+    if not os.path.exists(clone_dir):
+        display_message(f"Cloning {repo_name}...", Fore.GREEN)
+        try:
+            subprocess.run(["git", "clone", repo, clone_dir], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            display_message(f"Cloning completed successfully for {repo_name}.", Fore.GREEN)
+        except subprocess.CalledProcessError as e:
+            display_message(f"Error cloning {repo_name}. {e.stderr}", Fore.RED)
+    else:
+        display_message(f"{repo_name} is already cloned.", Fore.GREEN)
+
+    run_setup_scripts(clone_dir, repo_name)
+    install_requirements(clone_dir, repo_name)
+
 def main():
-    validate_input()
+    parser = argparse.ArgumentParser(description='Automate GitHub repository setup')
+    parser.add_argument('--base-directory', default='tools', help='Base directory for cloning repositories')
+    parser.add_argument('--log-file', default='github_tool_automation.log', help='Log file path')
+    parser.add_argument('--repositories', nargs='+', help='List of repository URLs')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(filename=args.log_file, level=logging.INFO)
+
+    repositories = args.repositories if args.repositories else [
+        "https://github.com/OWASP/Amass.git",
+        "https://github.com/zaproxy/zaproxy.git",
+        "https://github.com/aboul3la/Sublist3r.git",
+        "https://github.com/FortyNorthSecurity/EyeWitness.git",
+        "https://github.com/1N3/Sn1per.git",
+        "https://github.com/projectdiscovery/subfinder.git",
+        "https://github.com/m4ll0k/infoga.git",
+        "https://github.com/michenriksen/aquatone.git",
+        "https://github.com/smicallef/spiderfoot.git",
+        "https://github.com/lanmaster53/recon-ng.git"
+    ]
+
+    validate_input(repositories)
     selected_repositories = select_repositories(repositories)
 
-    for repo in selected_repositories:
-        repo_name = repo.split("/")[-1].replace(".git", "")
-        clone_dir = os.path.join(base_directory, repo_name)
+    os.makedirs(args.base_directory, exist_ok=True)
 
-        if not os.path.exists(clone_dir):
-            display_message(f"Cloning {repo_name}...", GREEN)
-            result = subprocess.run(["git", "clone", repo, clone_dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if result.returncode == 0:
-                display_message(f"Cloning completed successfully for {repo_name}.", GREEN)
-            else:
-                display_message(f"Error cloning {repo_name}.", RED)
-        else:
-            display_message(f"{repo_name} is already cloned.", GREEN)
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(clone_and_setup, selected_repositories, [args.base_directory] * len(selected_repositories))
 
-        # Check and run setup scripts
-        if not run_setup_script(clone_dir, repo_name):
-            display_message(f"No setup scripts found for {repo_name}.", GREEN)
-
-        # Check and install requirements
-        if not install_requirements(clone_dir, repo_name):
-            display_message(f"No requirements found for {repo_name}.", GREEN)
-
-    display_message("All selected repositories cloned and setup completed.", GREEN)
+    display_message("All selected repositories cloned and setup completed.", Fore.GREEN)
 
 if __name__ == "__main__":
     main()
